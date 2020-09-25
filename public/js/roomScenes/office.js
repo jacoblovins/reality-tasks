584f0d249f3e5769621d95eee627bcff4e5b9443
jacoblovins <lovins.jacob@yahoo.com>
2020-09-25 12:36:20 -0400

    added Kayla, tuned controls, and optimized

--- public/js/roomScenes/office.js
+++ public/js/roomScenes/office.js
@@ -46,7 +46,7 @@ function startOfficeRoom() {
   controls.lookVertical = false;
 
   const homeMaterial = new THREE.MeshBasicMaterial({ wireframe: false, transparent: true, opacity: 0 });
-  const homeGeometry = new THREE.PlaneGeometry(1.2, 3, 1);
+  const homeGeometry = new THREE.PlaneGeometry(2.2, 3.5, 1);
   const homeMesh = new THREE.Mesh(homeGeometry, homeMaterial);
   townScene.add(homeMesh);
 
@@ -176,6 +176,8 @@ function startOfficeRoom() {
       camera.position.z = 6.5;
     }else if(camera.position.z < -6.5){
       camera.position.z = -6.5;
+    } else if (camera.position.y < 1.7 || camera.position.y > 1.7) {
+      camera.position.y = 1.7;
     }
     render();
   };
